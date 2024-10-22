eddc5459bc5aab67a9194104c9312ef5bba7d0bd
Nathan Fisher <N.Fisher21@outlook.com>
2024-10-22 16:04:09 +0000

    Added and updated the base.html template and main-nav.html. Restructured directory after testing Django connectivity, Django is now connected.

--- narth_vad3r_store/settings.py
+++ narth_vad3r_store/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
+import os
+
+if os.path.isfile('env.py'):
+    import env
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -38,9 +43,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'products',
-    'users',
-    'orders',
-    'payments',
+#     'users',
+#     'orders',
+#     'payments',
 ]
 
 MIDDLEWARE = [
