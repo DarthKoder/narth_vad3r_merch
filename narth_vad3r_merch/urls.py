2912b22a18cb60fb8b51d1f7a7dd19ab4a4126c5
Nathan Fisher <N.Fisher21@outlook.com>
2024-11-20 18:52:32 +0000

    Tested account and allauth authentication and made minor changes to templates after minor issues. Added  stayling to all allauth forms for ux  and ui

--- narth_vad3r_merch/urls.py
+++ narth_vad3r_merch/urls.py
@@ -19,6 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
@@ -27,4 +28,5 @@ urlpatterns = [
     path('basket/', include('basket.urls')),
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
